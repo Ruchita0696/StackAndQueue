55e9ca5b15d999b0f59f95250930bd81e69e7cd6
ruchi06 <ruchi06@gmail.com>
2023-01-20 13:59:48 +0530

    Create Queue

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -18,6 +18,12 @@
             stack.Pop();
             stack.Display();
 
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
+
 
         }
     }
---
+++ StackAndQueue/Queue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class Queue
+    {
+        Node front;
+        Node rear;
+
+        public void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.front == null)
+            {
+                this.front = this.rear = newNode;
+            }
+            else
+            {
+                this.rear.next = newNode;
+                this.front = newNode;
+            }
+            Console.WriteLine($"{data} inserted into Queue");
+             
+        }
+        public void Display()
+        {
+            int i = 1;
+            int Size = 0;
+            Node temp = front;
+
+            Console.WriteLine();
+            if (temp == null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            while (temp != null)
+            {
+                Console.WriteLine($"Element {i} in stack is : " + temp.data);
+                temp = temp.next;
+                Size = i;
+                i++;
+            }
+            Console.WriteLine($"\nTotal Element in Stack is : {Size}\n");
+
+        }
+    }
+}
